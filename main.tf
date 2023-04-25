ad9417b0957015b4ce524074c0e7ceedf7a49e94
WUTTICHAI KONGPEAN <WUTTICHAIK@set.or.th>
2023-04-25 17:46:32 +0700

    aws_vpc main

--- main.tf
+++ main.tf
@@ -3,45 +3,45 @@ provider "aws" {
 }
 
 #Create vpc
-resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
+#resource "aws_vpc" "main" {
+# cidr_block = "10.0.0.0/16"
  
- tags = {
-   Name = "Project VPC"
- }
-}
+# tags = {
+#   Name = "Project VPC"
+# }
+#}
 
 #Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
+#resource "aws_security_group" "my_security_group" {
+#  name        = var.security_group
+#  description = "security group for Ec2 instance"
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port   = 8080
+#    to_port     = 8080
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
  # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  egress {
+#    from_port   = 0
+#    to_port     = 65535
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  tags= {
-    Name = var.security_group
-  }
-}
+#  tags= {
+#    Name = var.security_group
+#  }
+#}
 
 # Create AWS ec2 instance
 resource "aws_instance" "myFirstInstance" {
@@ -56,23 +56,23 @@ resource "aws_instance" "myFirstInstance" {
 }
 
 # Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
+#resource "aws_eip" "myFirstInstance" {
+#  vpc      = true
+#  instance = aws_instance.myFirstInstance.id
+#tags= {
+#    Name = "my_elastic_ip"
+#  }
+#}
 
 
 #Create route53
-resource "aws_route53_zone" "set-or-th" {
-  name = "set.or.th"
-}
-resource "aws_route53_record" "www-record" {
-  zone_id = aws_route53_zone.set-or-th.zone_id
-  name    = "www.set.or.th"
-  type    = "A"
-  ttl     = "300"
-  records = [aws_eip.myFirstInstance.public_ip]
-}
+#resource "aws_route53_zone" "set-or-th" {
+#  name = "set.or.th"
+#}
+#resource "aws_route53_record" "www-record" {
+#  zone_id = aws_route53_zone.set-or-th.zone_id
+#  name    = "www.set.or.th"
+#  type    = "A"
+#  ttl     = "300"
+#  records = [aws_eip.myFirstInstance.public_ip]
+#}
