78ec7e8d12831c3a43a90239663b24419b204550
WUTTICHAI KONGPEAN <WUTTICHAIK@set.or.th>
2023-04-26 10:42:06 +0700

    add ec2

--- main.tf
+++ main.tf
@@ -42,8 +42,11 @@ provider "aws" {
 #    Name = var.security_group
 #  }
 #}
-
-# Create AWS ec2 instance
+#Create route53
+resource "aws_route53_zone" "settrade-com" {
+  name = "settrade.com"
+}
+# Create AWS ec2 instance ----------------------------------------------------------------------------1
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
@@ -54,24 +57,21 @@ resource "aws_instance" "myFirstInstance" {
   subnet_id = var.subnet_id
   private_ip = "10.219.68.150"
   tags= {
-    Name = var.tag_name
+    Name = var.itppt-lb041
   }
 }
 
+
 # Create Elastic IP address
-#resource "aws_eip" "myFirstInstance" {
-#  vpc      = true
-#  instance = aws_instance.myFirstInstance.id
-#tags= {
-#    Name = "my_elastic_ip"
-#  }
-#}
-
-
-#Create route53
-resource "aws_route53_zone" "settrade-com" {
-  name = "settrade.com"
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
+  }
 }
+
+
 resource "aws_route53_record" "www-record" {
   zone_id = aws_route53_zone.settrade-com.zone_id
   name    = "wen041.settrade.com"
@@ -79,4 +79,38 @@ resource "aws_route53_record" "www-record" {
   ttl     = "300"
  # records = "10.219.68.150"
   records = [aws_instance.myFirstInstance.private_ip]
+}
+
+# Create AWS ec2 instance ----------------------------------------------------------------------------2
+resource "aws_instance" "myFirstInstance2" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  #security_groups= [var.security_group]
+  #security_groups= [var.security_group2]
+  vpc_security_group_ids = ["sg-0283926adc0c8f457"]
+  subnet_id = var.subnet_id
+  private_ip = "10.219.68.151"
+  tags= {
+    Name = var.itppt-lb042
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance2" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance2.id
+tags= {
+    Name = "my_elastic_ip"
+  }
+}
+
+
+resource "aws_route53_record" "www-record" {
+  zone_id = aws_route53_zone.settrade-com.zone_id
+  name    = "wen041.settrade.com"
+  type    = "A"
+  ttl     = "300"
+ # records = "10.219.68.151"
+  records = [aws_instance.myFirstInstance2.private_ip]
 }
